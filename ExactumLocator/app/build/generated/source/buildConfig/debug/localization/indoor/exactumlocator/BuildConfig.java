211bcb133798cf00b146a22bdf2a8bec1aa5dd16
Sebuli <sebastian.falk@helsinki.fi>
2015-12-27 16:36:29 +0200

    Change .gitignore again

---
+++ ExactumLocator/app/build/generated/source/buildConfig/debug/localization/indoor/exactumlocator/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package localization.indoor.exactumlocator;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "localization.indoor.exactumlocator";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
